77a15b8d02d03e11032a88c0be7ec2ba63c5bdba
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-03 21:48:56 -0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { ReactNode } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
       </Routes>
       <SpeedInsights />
     </BrowserRouter>
