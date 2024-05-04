b161f20287853899c8892bbdd2e9969eec67def2
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-03 21:26:19 -0500

    home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
