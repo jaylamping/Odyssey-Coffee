2b359f1d8b09b396a6a2473df423555836e0e6e0
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 14:17:00 -0500

    serif font

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div
-      className='w-full p-10 fixed top-0 left-0 z-50'
+      className='w-full p-10 fixed top-0 left-0 z-50 serif'
       style={{ height: '150px' }}
     >
       <div
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background'>
+    <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background serif'>
       <div className='col-start-2 col-span-6 flex items-center justify-center min-h-screen text-white pt-36'>
         <div>
           <h1 className='text-9xl'>Your Journey Starts Here.</h1>
-          <div className='space-x-12 pt-14 pl-10'>
+          <div className='space-x-12 pt-14 pl-4'>
             <Link
               className='px-10 py-3 bg-yellow-600 hover:bg-yellow-800 text-white font-bold rounded-xl'
               to='/menu'
