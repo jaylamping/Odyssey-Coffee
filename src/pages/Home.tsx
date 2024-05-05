db4c9c662bb72fc054aab76833eb332767997c5a
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 14:27:32 -0500

    added tailwind animate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background serif'>
-      <div className='col-start-2 col-span-6 flex items-center justify-center min-h-screen text-white pt-36'>
+      <div className='col-start-2 col-span-6 flex items-center justify-center min-h-screen text-white pt-36 animate-fade-up'>
         <div>
           <h1 className='text-9xl'>Your Journey Starts Here.</h1>
           <div className='space-x-12 pt-14 pl-4'>
