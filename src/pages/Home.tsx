ca5137cebe0e16bc1f3acf9b9df4330472547b90
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 04:10:48 -0500

    referring to className now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
 const Home = () => {
   return (
-    <div
-      className='bg-cover bg-center h-screen w-screen bg-no-repeat'
-      style={{ backgroundImage: `url('/main-background.png')` }}
-    >
+    <div className='bg-cover bg-center h-screen w-screen bg-no-repeat home-background'>
       <h1>Coffee is the yum yum.</h1>
     </div>
   );
