6faf3e1595f4ccf89be1c75ad669f5898b1ddf59
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 04:44:36 -0500

    new cta

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background'>
       <div className='col-start-4 col-span-4 flex items-center justify-center min-h-screen text-white pt-36'>
-        <h1 className='text-9xl'>Coffee is the yum yum.</h1>
+        <h1 className='text-9xl'>Your Journey Starts Here.</h1>
       </div>
     </div>
   );
