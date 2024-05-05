4b4ffa1bd7fb9cc33dbf11cd294e38a4613c7961
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 04:46:51 -0500

    fixing spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background'>
-      <div className='col-start-4 col-span-4 flex items-center justify-center min-h-screen text-white pt-36'>
+      <div className='col-start-3 col-span-5 flex items-center justify-center min-h-screen text-white pt-36'>
         <h1 className='text-9xl'>Your Journey Starts Here.</h1>
       </div>
     </div>
