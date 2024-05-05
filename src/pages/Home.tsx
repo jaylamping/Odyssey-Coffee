44d38915703f8822d000c7bcb151c93795a7244e
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 05:51:12 -0500

    lol this blue does not look good

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,17 @@ const Home = () => {
       <div className='col-start-2 col-span-6 flex items-center justify-center min-h-screen text-white pt-36'>
         <div>
           <h1 className='text-9xl'>Your Journey Starts Here.</h1>
-          <div className='space-x-12 pt-8'>
-            <Link className='px-8' to='/menu'>
+          <div className='space-x-12 pt-14'>
+            <Link
+              className='px-10 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+              to='/menu'
+            >
               View Menu
             </Link>
-            <Link className='px-8' to='/order'>
+            <Link
+              className='px-10 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+              to='/order'
+            >
               Order Ahead
             </Link>
           </div>
