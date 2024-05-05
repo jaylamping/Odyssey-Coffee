0712fa1cced5ed2cb363b7a0e03683e1a940ebd2
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 05:29:35 -0500

    getting better

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <div className='grid grid-cols-12 bg-cover bg-center h-screen w-screen bg-no-repeat home-background'>
-      <div className='col-start-3 col-span-5 flex items-center justify-center min-h-screen text-white pt-36'>
-        <h1 className='text-9xl'>Your Journey Starts Here.</h1>
+      <div className='col-start-2 col-span-6 flex items-center justify-center min-h-screen text-white pt-36'>
+        <div>
+          <h1 className='text-9xl'>Your Journey Starts Here.</h1>
+          <div className='space-x-12 pt-8'>
+            <Link className='px-8' to='/menu'>
+              View Menu
+            </Link>
+            <Link className='px-8' to='/order'>
+              Order Ahead
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
