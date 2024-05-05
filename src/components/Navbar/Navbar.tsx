fdcf9938075248da2717c8eb1e8486e3d2a99b8d
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-05-05 01:42:52 -0500

    backgrounds and margins

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div
-      className='w-full p-12 fixed top-0 left-0 z-50'
+      className='w-full p-10 fixed top-0 left-0 z-50'
       style={{ height: '150px' }}
     >
       <div
         className='grid grid-cols-12 items-center'
         style={{ height: '100%' }}
       >
-        <div className='col-span-4 flex justify-between items-center pl-14'>
+        <div className='col-span-4 flex justify-between items-center pl-16'>
           <Link className='nav-link' to='/'>
             Home
           </Link>
@@ -25,6 +25,7 @@ const Navbar = () => {
           <img
             src='/logo.svg'
             alt='Logo'
+            className='white-logo'
             style={{
               height: '150px',
               width: 'auto',
@@ -34,7 +35,7 @@ const Navbar = () => {
             }}
           />
         </div>
-        <div className='col-span-4 flex justify-between items-center pr-14'>
+        <div className='col-span-4 flex justify-between items-center pr-16'>
           <Link className='nav-link' to='/order'>
             Order Online
           </Link>
